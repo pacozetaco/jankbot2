a7b29dc212891739fcb44079ad24b70b7c402ba2
smythbrjr <smyth.brianjr@gmail.com>
2024-10-20 20:29:24 +0000

    non blocking ai chat

--- main.py
+++ main.py
@@ -2,47 +2,35 @@ import config, discord, utils.ark.config_uploader as config_uploader
 from discord.ext import commands
 from utils.ark.arkinfo import ArkInfo, ArkRcon
 from games.baccarat import BaccaratManager
-from utils.aichat import chat
-
-#print("Importing required modules...", flush=True)
-#print("Modules imported successfully", flush=True)
+from utils.aichat import process_ai_request
 
 class JankBot(commands.Bot):
-    #print("Creating bot instance...", flush=True)
     def __init__(self):
         super().__init__(
             command_prefix="!",
             intents=discord.Intents.all()
         )
-        #print("Bot initialized successfully", flush=True)
+
     
     #load cogs
     async def setup_hook(self):
-        #print("Loading cogs...", flush=True)
+
         await self.load_extension('cogs.pitboss')
-        opus_path = '/usr/lib/libopus.so.0.10.1'  # apk add --no-cache opus-dev
+        opus_path = '/usr/lib/libopus.so.0.10.1'  
         try:
             discord.opus.load_opus(opus_path)
-            #print("Opus loaded successfully", flush=True)
         except Exception as e:
             print(f"Error loading Opus: {e}", flush=True)
 
     async def on_message(self, message):
         if message.author.bot:
-            #print("Ignoring bot messages...", flush=True)
             return
-        #print("Processing commands...", flush=True)
         await self.process_commands(message) 
         if message.channel.name == "ai-chat":
-            print(message.content)
-            ai_message = await message.reply("```Thinking...```")
-            response = await chat(message.content)
-            await ai_message.edit(content=f"```{response}```")
+            await process_ai_request(message)
         if message.channel.id == int(config.ARK_CONFIG_CHANNEL):
-            #print("Uploading config to channel...", flush=True)
             await config_uploader.upload_config(message)
         if message.channel.id == int(config.ARK_CHAT_CHANNEL):
-            #print("Executing Rcon command...", flush=True)
             rcon = ArkRcon(f"ServerChat {str(message.author)} {str(message.content)}")
             rcon.execute_command()
     
@@ -50,11 +38,9 @@ class JankBot(commands.Bot):
         print("Logged in as", self.user, flush=True)
         # Attempt to initialize ArkInfo
         try:
-            #print("Initializing ArkInfo...", flush=True)
             chat_channel = self.get_channel(int(config.ARK_CHAT_CHANNEL))
             channel = self.get_channel(int(config.ARK_STATUS_CHANNEL))
             if channel != None and chat_channel != None:
-                #print("ArkInfo initialized successfully", flush=True)
                 await channel.purge(limit=None)
                 self.loop.create_task(ArkInfo.start_loop(self, channel, chat_channel))
             else:
@@ -67,25 +53,19 @@ class JankBot(commands.Bot):
             for channel in guild.channels:
                 if channel.name.lower().startswith("baccarat"):
                     self.loop.create_task(BaccaratManager.start_manager(self, channel))
-        
-
-
+    
         # Attempt to initialize Jukebox
         try:
-          #  print("Initializing Jukebox...", flush=True)
             jukebox_channel = self.get_channel(int(config.JUKEBOX_INFO_CHANNEL))
             if jukebox_channel != None:
                 from cogs import jukebox
-              #  print("Jukebox initialized successfully", flush=True)
                 self.loop.create_task(jukebox.setup(self, jukebox_channel))
             else:
                 print("Failed to initialize Jukebox: Channel not found", flush=True)
         except Exception as e:
             print(f"Error initializing Jukebox: {e}", flush=True)
 
-
 #main entry
-#print("Running bot with token...", flush=True)
 if __name__ == "__main__":
     JankBot().run(config.DISCORD_TOKEN)
     print("Bot started successfully", flush=True)
--- utils/aichat.py
+++ utils/aichat.py
@@ -1,14 +1,24 @@
-import ollama
+import ollama, asyncio
+from concurrent.futures import ThreadPoolExecutor
 
 # Define the URL for your Ollama instance or API endpoint
-OLLAMA_URL = "http://127.0.0.1:11434"  # Replace with your actual URL if different
 
-async def chat(prompt):
+OLLAMA_URL = "http://127.0.0.1:11434" 
+
+def chat(prompt):
     response = ollama.chat(
-    model="llama2-uncensored",  # Replace with your desired model
+    model="llama2-uncensored",  
     messages=[
         {"role": "user", "content": prompt}
     ],
     options={"num_predict": 300},
 )
     return response['message']['content']
+
+async def process_ai_request(message):
+    executor = ThreadPoolExecutor()
+    ai_message = await message.reply(content="```Thinking...```")
+    loop = asyncio.get_event_loop()
+    response = await loop.run_in_executor(executor, chat, message.content)
+    await ai_message.edit(content=f"```{response}```")
+    executor.shutdown()
